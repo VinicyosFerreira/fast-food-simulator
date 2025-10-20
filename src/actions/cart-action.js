3da73a69559f8311f37b4f3221b9972ab0272042
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-20 16:06:13 -0300

    feat: add reducers and actions to increment, decrement and calculate total cart

--- src/actions/cart-action.js
+++ src/actions/cart-action.js
@@ -3,11 +3,49 @@ export const ADD_CART = (dispatch, optionType) => {
     type: 'ADD_CART',
     payload: optionType,
   });
+  dispatch({
+    type: 'CALCULATE_TOTAL',
+    payload: optionType,
+  });
 };
 
 export const REMOVE_CART = (dispatch, optionType) => {
   dispatch({
     type: 'REMOVE_CART',
+    payload: optionType,
+  });
+  dispatch({
+    type: 'CALCULATE_TOTAL',
+    payload: optionType,
+  });
+};
+
+export const INCREMENT_CART = (dispatch, optionType) => {
+  dispatch({
+    type: 'INCREMENT_CART',
+    payload: optionType,
+  });
+  dispatch({
+    type: 'CALCULATE_TOTAL',
+    payload: optionType,
+  });
+};
+
+export const DECREMENT_CART = (dispatch, optionType) => {
+  dispatch({
+    type: 'DECREMENT_CART',
+    payload: optionType,
+  });
+
+  if (optionType.quantity === 1) {
+    dispatch({
+      type: 'REMOVE_CART',
+      payload: optionType,
+    });
+  }
+
+  dispatch({
+    type: 'CALCULATE_TOTAL',
     payload: optionType,
   });
 };
--- src/reducer/CartReducer.js
+++ src/reducer/CartReducer.js
@@ -35,14 +35,56 @@ export const cartReducer = (state, action) => {
       };
     }
 
-    // Remover produto do carrinho
     case 'REMOVE_CART':
-      console.log(state.cart);
+      // Remover produto do carrinho
       return {
         ...state,
         cart: state.cart.filter((item) => {
           return item.id !== action.payload.id;
         }),
       };
+
+    case 'INCREMENT_CART':
+      // Incrementar o produto em 1 quantidade
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+
+          return item;
+        }),
+      };
+
+    case 'DECREMENT_CART':
+      // Decrementar o produto em 1 quantidade
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              quantity: item.quantity > 1 ? item.quantity - 1 : item.quantity,
+            };
+          }
+
+          return item;
+        }),
+      };
+
+    case 'CALCULATE_TOTAL': {
+      // Calcular o total do carrinho
+      const total = state.cart.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0);
+      return {
+        ...state,
+        total,
+      };
+    }
   }
 };
