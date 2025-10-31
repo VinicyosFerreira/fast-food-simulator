0f3b54a68d0d5d943de8991fde8af88977a81f89
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-31 18:05:56 -0300

    feat: add fields and labels to the CustomerData and MethodPayment steps

--- src/components/CustomerData.jsx
+++ src/components/CustomerData.jsx
@@ -1,11 +1,66 @@
 import Button from '../components/Button';
+import Input from './Input';
 
-const CustomerData = ({ nextStep, prevStep }) => {
+const CustomerData = ({ nextStep, prevStep, handleFields }) => {
   return (
-    <div>
-      <h2>Customer Data</h2>
-      <Button onClick={() => nextStep()}>Avançar</Button>
-      <Button onClick={() => prevStep()}>Voltar</Button>
+    <div className="mx-auto flex w-[450px] flex-col items-center justify-center gap-2 rounded-md bg-foreground/10 p-5">
+      <Input
+        label="Nome"
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Digite seu nome"
+        onChange={(e) => handleFields('name', e.target.value)}
+      />
+
+      <Input
+        label="CPF"
+        type="text"
+        id="cpf"
+        name="cpf"
+        placeholder="Digite seu CPF"
+        onChange={(e) => handleFields('cpf', e.target.value)}
+      />
+
+      <Input
+        label="CEP"
+        type="text"
+        id="cep"
+        name="cep"
+        placeholder="Digite seu CEP"
+        onChange={(e) => handleFields('cep', e.target.value)}
+      />
+
+      <div className="flex w-full gap-2">
+        <Input
+          label="Endereço"
+          type="text"
+          id="address"
+          width="w-[70%]"
+          name="address"
+          placeholder="Digite seu endereço"
+          onChange={(e) => handleFields('address', e.target.value)}
+        />
+
+        <Input
+          label="Número"
+          type="text"
+          width="w-[30%]"
+          id="number"
+          name="number"
+          onChange={(e) => handleFields('number', e.target.value)}
+          placeholder="Número"
+        />
+      </div>
+
+      <div className="mt-2 flex w-full items-center justify-between">
+        <Button onClick={() => nextStep()} className="w-[200px]">
+          Avançar
+        </Button>
+        <Button onClick={() => prevStep()} className="w-[200px]">
+          Voltar
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -1,11 +1,109 @@
+import { FaPix } from 'react-icons/fa6';
+import { BsCreditCard2BackFill } from 'react-icons/bs';
+import { GiMoneyStack } from 'react-icons/gi';
+import Input from './Input';
 import Button from '../components/Button';
 
-const PaymentMethod = ({ prevStep }) => {
+const PaymentMethod = ({ prevStep, handleFields, fields }) => {
   return (
-    <div>
-      <h2>Metodo de pagamento</h2>
-      <Button>Concluir compra</Button>
-      <Button onClick={() => prevStep()}>Voltar</Button>
+    <div className="mx-auto flex w-[450px] flex-col gap-2 rounded-md bg-foreground/10 p-5">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2">
+          <FaPix className="text-2xl text-green-700" />
+          <label htmlFor="pix">Pix</label>
+        </div>
+        <input
+          type="radio"
+          id="pix"
+          name="payment-methods"
+          value={'pix'}
+          className="size-4"
+          onChange={(e) => handleFields('paymentMethod', e.target.value)}
+        />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2">
+          <BsCreditCard2BackFill className="text-2xl text-blue-800" />
+          <label htmlFor="credit-card">Cartão de crédito</label>
+        </div>
+        <input
+          type="radio"
+          id="credit-card"
+          value={'card'}
+          name="payment-methods"
+          className="size-4"
+          onChange={(e) => handleFields('paymentMethod', e.target.value)}
+        />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2">
+          <GiMoneyStack className="text-2xl text-green-800" />
+          <label htmlFor="money">Dinheiro</label>
+        </div>
+        <input
+          type="radio"
+          id="money"
+          value={'money'}
+          name="payment-methods"
+          className="size-4"
+          onChange={(e) => handleFields('paymentMethod', e.target.value)}
+        />
+      </div>
+
+      {fields?.paymentMethod === 'card' && (
+        <div className="flex flex-col gap-2">
+          <Input
+            type="text"
+            id="card-number"
+            label="Número do cartão"
+            name="card-number"
+            placeholder="Digite o número do cartão"
+            onChange={(e) => handleFields('card-number', e.target.value)}
+          />
+
+          <Input
+            type="text"
+            id="card-holder"
+            name="card-holder"
+            label="Titular do cartão"
+            placeholder="Digite o titular do cartão"
+            onChange={(e) => handleFields('owner-card', e.target.value)}
+          />
+
+          <div className="flex w-full gap-2">
+            <Input
+              label="Data de vencimento"
+              type="date"
+              id="expiration-date"
+              width="w-[70%]"
+              name="expiration-date"
+              onChange={(e) => handleFields('expiration-date', e.target.value)}
+              placeholder="Data de vencimento"
+            />
+
+            <Input
+              label="CVV"
+              type="text"
+              width="CVV"
+              id="card-cvv"
+              name="card-cvv"
+              onChange={(e) => handleFields('card-cvv', e.target.value)}
+              placeholder="CVV"
+            />
+          </div>
+        </div>
+      )}
+
+      <div className="mt-2 flex w-full items-center justify-between">
+        <Button className="w-[200px]" type="submit">
+          Concluir compra
+        </Button>
+        <Button className="w-[200px]" onClick={() => prevStep()}>
+          Voltar
+        </Button>
+      </div>
     </div>
   );
 };
