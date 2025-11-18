8c234f7ec54426f87b73dfcd1e9c978e703e72ef
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-18 15:43:02 -0300

    feat: change the Banner image and the structure Slogan component

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -1,15 +1,13 @@
-import imgSlogan from '../assets/imgs/img-slogan.png';
+import slogan from '../assets/imgs/slogan.png';
 
 const Slogan = () => {
   return (
-    <div className="flex h-[350px] items-center rounded-md bg-primary text-foreground">
-      <div className="break-word flex flex-col px-2 text-2xl font-semibold md:p-5 md:text-6xl">
-        <h2>Uma explosão de experiências </h2>
-        <h2>e sabores inesquecíveis !</h2>
-      </div>
-      <div className="w-[90%] md:w-[500px]">
-        <img src={imgSlogan} alt="Imagem do Slogan" />
-      </div>
+    <div>
+      <img
+        src={slogan}
+        alt="Banner do Burguermino"
+        className="h-[375px] w-full object-fill md:h-auto"
+      />
     </div>
   );
 };
